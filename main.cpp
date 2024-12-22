9cc2ced77df39df3df0a780278a1ec980c2fbe8d
VAlduinV <prime72w@gmail.com>
2024-12-22 16:10:05 +0200

    add 'Hello, world! Again:)'

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//
+// Created by prime on 22.12.2024.
+//
+#include <iostream>; // preprocessor
+using namespace std;
+
+int main() {
+    // statements
+    int x; // statement declaration
+    x = 5; // assignment statement
+    cout << x; // output statement
+
+    cout << "Hello, world!";
+
+    return 0;
+}
